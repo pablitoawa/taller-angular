2fdc8ed976ac041b33851a0d807e9e00d1bd6ffc
pablitoawa <pablo77080302@hotmail.com>
2024-05-08 17:13:13 -0500

    details-base

--- src/app/components/productos-detalles/productos-detalles.component.ts
+++ src/app/components/productos-detalles/productos-detalles.component.ts
@@ -1,28 +1,34 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductosService } from '../../services/productos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-productos-detalles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './productos-detalles.component.html',
-  styleUrl: './productos-detalles.component.css'
+  styleUrl: './productos-detalles.component.css',
 })
 export class ProductosDetallesComponent {
-
   ruta = inject(ActivatedRoute);
-
   servicios = inject(ProductosService);
   productos: any;
+  detalles: any;
+  id: any;
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.servicios.getProductos().subscribe((data) => { this.productos = data });
-
-    
+    this.ruta.params.subscribe((params) => {
+      console.log(params);
+      this.id = params['idProductos'];
+      console.log(this.id);
+      this.servicios.getProductos().subscribe((data) => {
+        this.productos = data;
+        this.detalles = this.productos.find((item: any) => item.id == this.id);
+        console.log(this.detalles);
+      });
+    });
   }
-
-  
 }
