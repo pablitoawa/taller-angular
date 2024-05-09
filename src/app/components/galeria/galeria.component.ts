84d6e522a4ad71ea0266603a6eb9f77c7327976a
pablitoawa <pablo77080302@hotmail.com>
2024-05-08 19:06:26 -0500

    filter-price

--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -3,11 +3,12 @@ import { Component, inject } from '@angular/core';
 import { ProductosService } from '../../services/productos.service';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Options, LabelType, NgxSliderModule } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, NgxSliderModule],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.css'
 })
@@ -22,4 +23,20 @@ export class GaleriaComponent {
     this.servicios.getProductos().subscribe((data) => { this.productos = data })
   }
 
+  minValue: number = 100;
+  maxValue: number = 400;
+  options: Options = {
+    floor: 0,
+    ceil: 1000,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return "<b>Min price:</b> $" + value;
+        case LabelType.High:
+          return "<b>Max price:</b> $" + value;
+        default:
+          return "$" + value;
+      }
+    }
+  };
 }
