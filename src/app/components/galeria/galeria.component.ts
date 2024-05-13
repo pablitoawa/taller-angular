329be201a2551a202fa3b0e45d99db348e7c2fcc
pablitoawa <pablo77080302@hotmail.com>
2024-05-13 07:28:53 -0500

    category filter

--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -39,4 +39,7 @@ export class GaleriaComponent {
       }
     }
   };
+
+  category: string = "";
+  
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -3,15 +3,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductosService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private API_PRODUCTOS = 'http://localhost:3000/productos';
 
-  getProductos():Observable <any> {
+  getProductos(): Observable<any> {
+    return this.http.get(this.API_PRODUCTOS);
+  }
+
+  getCategory(category : string): Observable<any> {
     return this.http.get(this.API_PRODUCTOS);
   }
 }
