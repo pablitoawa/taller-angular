99e073a2b0c51803d42bc52b99df8ffba95177b3
pablitoawa <pablo77080302@hotmail.com>
2024-05-15 18:50:20 -0500

    respaldo

--- src/app/pages/productos/productos.component.ts
+++ src/app/pages/productos/productos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GaleriaComponent } from '../../components/galeria/galeria.component';
+import { CartComponent } from '../../components/cart/cart.component';
 
 @Component({
   selector: 'app-productos',
   standalone: true,
-  imports: [GaleriaComponent],
+  imports: [GaleriaComponent, CartComponent],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
