f6a0744f9b925192359a76dbcb95e3c1fe520a4e
Jonathan5441 <jonathanc4331@gmail.com>
2024-05-23 15:22:39 -0500

    Cat2

--- src/app/guards/editor-productos.guard.ts
+++ src/app/guards/editor-productos.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn } from '@angular/router';
 
 export const editorProductosGuard: CanActivateFn = (route, state) => {
   const rol = localStorage.getItem('rol')
-  if (rol == 'administrador' || rol == 'usuario') {
+  if (rol == 'administrador') {
     return true;
   } else {
     return false;
